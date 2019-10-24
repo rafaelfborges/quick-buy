9209344c88b7d660f6bc369d1414ee3040fd7e12
Rafael Borges <rafaelfborges@gmail.com>
2019-10-24 01:58:51 -0300

    Reconfiguração projeto .netcore 2.2 e Criação das  Migrations

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto : Entidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+        public override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
