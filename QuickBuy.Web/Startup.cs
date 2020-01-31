3cfb6f7bf23638b27746a65fc7afcbe52e9456f0
Rafael Borges <rafaelfborges@gmail.com>
2020-01-31 11:54:32 -0300

    fix: Setting up DB connection string

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -28,9 +28,10 @@ namespace QuickBuy.Web
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             var connectionString = Configuration.GetConnectionString("QuickBuyDB");
-            services.AddDbContext<QuickBuyContexto>(option =>
-                                                        option.UseLazyLoadingProxies()
-                                                        .UseMySql(connectionString,
+            services.AddDbContext<QuickBuyContexto>(options =>
+                                                        options
+                                                            .UseLazyLoadingProxies()
+                                                            .UseMySql(connectionString,
                                                                             m => m.MigrationsAssembly("QuickBuy.Repositorio")));
 
             // In production, the Angular files will be served from this directory
