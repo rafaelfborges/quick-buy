e7e8e160e0612a6f5cb6dfb074cd19b4b919d671
Rafael Borges <rafaelfborges@gmail.com>
2020-01-31 13:30:11 -0300

    feat: Add ProdutoController to Web Api

---
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+    }
+}
